8875788565b8e028db178ae5987886ba4bb7dda9
fruitegg <matthewsfox33@hotmail.com>
2017-06-10 14:14:25 -0600

    Create hackerrank4.py

--- hackerrank4.py
+++ hackerrank4.py
@@ -1,15 +1,10 @@
-#!/bin/python3
-
-import sys
-from functools import reduce 
+from functools import reduce
 
 def getPrimes():
     ''' Inefficient prime algorithm I had already made '''
     upperBound = 10000
-
     #Our possible primes are odds from 3 to 200,000
     possiblePrimes = [2] + list(range(3,upperBound,2))
-
     #Go through all of the possible factors of any of those numbers (3 to 447)
     for i in range(3,1000):
         if i in possiblePrimes:
@@ -18,7 +13,6 @@ def getPrimes():
     return possiblePrimes
 
 def getDivisors(num):
-    #TODO: make it output the divisors in order
     ''' Gets all of the divisors of a given number '''
     divs = []
     for i in range(1, int(1 + num ** .5)):
@@ -30,15 +24,18 @@ def getDivisors(num):
     return divs
 
 def gcd(a,b):
-    ''' Finds the greatest common divisor of two integers. ''''
+    ''' Finds the greatest common divisor of two integers. '''
     if a < b: # require a > b
-        A,b,a  = b,a,A
+        A = b
+        b,a = a,A
     while b != 0:
         a, b = b, a % b
     return a
 
 def totients(n):
     ''' Computes the totient function using the greatest common divisor function. '''
+    if n == 1:
+        return 1
     totients = 0
     for i in range(1, n):
         if gcd(n,i) == 1:
@@ -61,21 +58,11 @@ def sumTotientTree(n,d):
     return sum(totientTree)
 
 def divisorExploration(m, a, d):
-    ''' '''
     #p[i] is the ith prime, which means we need m primes
     #we know that m has an upper bound of 1000,
     #so we just need the first 1000 primes
     #we do this with a sieve up to n = 10000
-    p = [1] + getPrimes() #adding 1 makes sure the indices are correct
-    L = [p[i]** a + i for i in range(1, m+1)]#should go from 1 to m
+    p = [1] + getPrimes() # adding 1 makes sure the indices are correct
+    L = [p[i]** (a + i) for i in range(1, m+1)]#should go from 1 to m
     n = reduce(lambda x, y: x*y, L)
     return sumTotientTree(n,d) % (10**9 + 7)
-    
-
-q = int(input().strip())
-for a0 in range(q):
-    m, a, d = input().strip().split(' ')
-    m, a, d = [int(m), int(a), int(d)]
-    result = divisorExploration(m, a, d)
-    print(result)
-    
