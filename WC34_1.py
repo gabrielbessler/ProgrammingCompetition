bb0fe977aa0e9859ad9b5d4af77dde401bcb7845
Gabriel Bessler <gabrielbessler@gmail.com>
2017-07-19 16:21:22 -0600

    Added problem statements and cleaned up code

--- WC34_1.py
+++ WC34_1.py
@@ -1,17 +1,25 @@
 #!/bin/python3
 
 import sys
-from functools import reduce 
+from functools import reduce
 
-def onceInATram(x):
-    # Complete this function
+'''
+Given Jack's current ticket number, find and print the next lucky ticket number
+
+A lucky ticket is a six-digit number on the ticket in which sum of the first three
+digits is equal to the sum of the last three digits (in this case, we are only dealing
+with six digit numbers).
+'''
+
+def nextLuckyNumber(curr_val):
+
     while True:
-        x_str = str(x) 
-        if reduce(lambda x,y: int(x) + int(y), x_str[:3]) == reduce(lambda x,y: int(x) + int(y), x_str[3:]):
-            return x
-        x+=1
+        curr_val_str = str(starting_val)
+        if reduce(lambda x, y: int(x) + int(y), curr_val_str[:3]) == reduce(lambda x, y: int(x) + int(y), curr_val_str[3:]):
+            return curr_val
+        curr_val += 1
 
 if __name__ == "__main__":
-    x = int(input().strip())
-    result = onceInATram(x+1)
+    starting_val = int(input().strip())
+    result = nextLuckyNumber(starting_val+1)
     print(result)
--- WC34_2.py
+++ WC34_2.py
@@ -1,8 +1,17 @@
 #!/bin/python3
 
-import sys
+'''
+Given lists A and B, find the pair (x,y) such that
+
+1. x is in A, y is in B
+2. gcd(x,y) is the maximum out of all pairs (x,y)
+3. If there is a tie for the gcd, pick the pair with the greatest sum.
+
+After you have found this pair, return the sum.
+'''
 
 def maximumGcdAndSum(A, B):
+    ''' TODO '''
 
     #We know that we cannot have a GCD higher than the maximum of a set
     maxA, maxB = max(A), max(B)
@@ -19,18 +28,18 @@ def maximumGcdAndSum(A, B):
         x = possible_gcd * (highest_val // possible_gcd)
         fnum = 0
         snum = 0
-        while x >= 1 :
-            if fnum == 0: 
+        while x >= 1:
+            if fnum == 0:
                 if x in A:
                     fnum = x
-            if snum == 0: 
+            if snum == 0:
                 if x in B:
                     snum = x
             #We are going through each possible multiple of the GCD that will fit in the set
             x -= possible_gcd
 
         if fnum != 0 and snum != 0:
-            return (fnum + snum)
+            return fnum + snum
 
 if __name__ == "__main__":
     n = int(input().strip())
