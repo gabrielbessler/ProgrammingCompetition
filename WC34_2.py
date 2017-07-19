f8e9d80976575dcd2ce771afd366c5c6f4d0f208
Gabriel Bessler <gabrielbessler@gmail.com>
2017-07-19 11:57:30 -0600

    Final Solution
    By changing the input data type from list -> set, we go from O(n) (average) to O(1) (average) for the 
    (element in Set)
    operation, which is the bulk of the work done in this problem.

--- WC34_2.py
+++ WC34_2.py
@@ -5,23 +5,29 @@ import sys
 def maximumGcdAndSum(A, B):
 
     #We know that we cannot have a GCD higher than the maximum of a set
-    maximum_possible_factor = min(max(A), max(B))
-    highest_val = max(max(A), max(B))
+    maxA, maxB = max(A), max(B)
+    maximum_possible_factor = min(maxA, maxB)
+    highest_val = max(maxA, maxB)
+
+    A = set(A)
+    B = set(B)
 
     #We are going to start with the highest possible GCD and work our way down
     for possible_gcd in range(maximum_possible_factor, 0, -1):
 
         #For this given GCD to be our answer, it has to have a multiple in A and a multiple in B
-        x = possible_gcd
+        x = possible_gcd * (highest_val // possible_gcd)
         fnum = 0
         snum = 0
-        while x <= highest_val:
-            if x in A:
-                fnum = x
-            if x in B:
-                snum = x
+        while x >= 1 :
+            if fnum == 0: 
+                if x in A:
+                    fnum = x
+            if snum == 0: 
+                if x in B:
+                    snum = x
             #We are going through each possible multiple of the GCD that will fit in the set
-            x += possible_gcd
+            x -= possible_gcd
 
         if fnum != 0 and snum != 0:
             return (fnum + snum)
