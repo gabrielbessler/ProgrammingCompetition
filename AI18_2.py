d3b9d9f61c2c52d35c438ededf55abc7e4137121
Gabriel Bessler <gabrielbessler@gmail.com>
2017-08-05 20:08:31 -0600

    Update AI18_2.py

--- AI18_2.py
+++ AI18_2.py
@@ -4,29 +4,66 @@ import sys
 import time
 from math import log
 
-def towerColoring(n):
+'''
+Problem Statement:
+
+For a given integer n, there is a tower built from 3**n blocks stacked vertically.
+Each of these blocks can be colored in 3 different colors: red, green or blue.
+How many different colorings of the tower can be created? Two colorings are considered
+different if and only if there exists at least one block with different colors in the colorings.
+Since the result can be a huge number, apply a modulo 10**9 + 7 on the result.
+
+My Solution:
+
+If there are k blocks, and each block has 3 possible colors, then we have 3**k possible colorings.
+We know that k = 3**n, s.t. the number of colorings C is
+
+C = 3**3**n
+
+'''
+
+def towerColoring(num):
+    '''
+
+    '''
     total = 3
-    while n > 1:
+    while num > 1:
         total *= 3
         total %= (10**9 + 7)
-        n -= 1
-    newTotal = 3
+        num -= 1
+    new_total = 3
     while total > 1:
-        newTotal *= 3
-        newTotal %= (10**9 + 7)
+        new_total *= 3
+        new_total %= (10**9 + 7)
         total -= 1
-    return newTotal
+    return new_total
 
-def towerColoring2(n):
-    return (3 ** (3 ** n)) % (10**9 + 7)
+def towerColoring2(num):
+    '''
+    Naive approach to solving 3**3**n mod 10**9 + 7,
+    by simply computing the value and then taking its modulo. 
+    '''
+    return (3 ** (3 ** num)) % (10**9 + 7)
 
-def findPower(base, exp, mod = 0):
-    binaryPowerRep = bin(exp)[2:]
-    binaryDigitParts = []
-    for i in range(len(binaryPowerRep)):
-        binaryDigitParts += [(2 ** (len(binaryPowerRep) - 1 - i)) * int(binaryPowerRep[i])]
+def towerColoring3(num):
+    '''
+    This is the fast algorithm we use.
+    '''
+    step_one = int(findPower(3, num, 10**9 + 6))
+    step_two = findPower(3, step_one, 10**9 + 7)
+    return step_two
+
+def findPower(base, exp, mod=0):
+    '''
+    Fast algorithm for finding 
+    (base ** exp) % mod
+    '''
+    binary_power_rep = bin(exp)[2:]
+    binary_digit_parts = []
+    for i in range(len(binary_power_rep)):
+        binary_digit_parts += [(2 ** (len(binary_power_rep) - 1 - i)) * int(binary_power_rep[i])]
     totals = []
-    for power in binaryDigitParts:
+    for power in binary_digit_parts:
         if power != 0:
             value = base
             for j in range(int(log(power,2))):
@@ -44,30 +81,27 @@ def findPower(base, exp, mod = 0):
     else:
         return total % mod
 
-def towerColoring3(n):
-    stepOne = int(findPower(3, n, 10**9 + 6))
-    stepTwo = findPower(3, stepOne, 10**9 + 7)
-    return stepTwo
-
-#n = int(input().strip())
-#result = towerColoring(n)
-#print(result)
+n = int(input().strip())
+result = towerColoring3(n)
+print(result)
 
 '''
-testNum = 16
+Speed Comparison
+
+test_num = 16
 
 st = time.time()
-print(towerColoring(testNum))
+print(towerColoring(test_num))
 print("First Trial: " + str(time.time() - st))
 print("============================")
 
 st = time.time()
-print(towerColoring2(testNum))
+print(towerColoring2(test_num))
 print("Second Trial: " + str(time.time() - st))
 print("============================")
 
 st = time.time()
-print(towerColoring3(testNum))
+print(towerColoring3(test_num))
 print("Third Trial: " + str(time.time() - st))
 print("============================")
 '''
